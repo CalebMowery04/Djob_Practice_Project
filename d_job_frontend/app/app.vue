623f7c1b19061ee694e8f7a167ddaac1d3d10f6c
CalebMowery04 <calebmowery7@gmail.com>
2026-05-20 16:34:07 -0400

    first commit

---
+++ d_job_frontend/app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+  <!--
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+  -->
+</template>
